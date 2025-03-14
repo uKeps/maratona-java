9e2d52515bcdac2147c2b4615099fd31a3a75568
uKeps <piassinierik@gmail.com>
2025-03-13 22:29:48 -0300

    Aulas feitas dia 13/03

---
+++ src/maratona/Aula05EstruturasCondificonais04Exercicio.java
@@ -0,0 +1,19 @@
+package maratona;
+
+public class Aula05EstruturasCondificonais04Exercicio {
+    public static void main(String[] args) {
+        // i want to know how much I have to pay in the Netherlands 2020 based on my annual salary
+        double salary = 25000;
+        double tax;
+
+        if (salary <= 34712) {
+            tax = 0.097;
+        } else if (salary >= 34713 && salary <= 68507) {
+            tax = 3.735;
+        } else {
+            tax = 4.950;
+        }
+        double salaryAfterTax = salary * tax;
+        System.out.println(salary - salaryAfterTax);
+    }
+}
