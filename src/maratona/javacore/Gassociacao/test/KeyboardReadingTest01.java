9b50d6629f29d9eb5ce9ffcdd1fb169ce2bfd51c
uKeps <piassinierik@gmail.com>
2025-03-25 22:17:52 -0300

    feature: usando o scanner

---
+++ src/maratona/javacore/Gassociacao/test/KeyboardReadingTest01.java
@@ -0,0 +1,21 @@
+package maratona.javacore.Gassociacao.test;
+
+import java.util.Scanner;
+
+public class KeyboardReadingTest01 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("What's your name? ");
+        String name = sc.nextLine();
+        System.out.println("What's your age? ");
+        int age = sc.nextInt();
+        System.out.println("What's your gender? (F or M) ");
+        char gender = sc.next().charAt(0);
+        System.out.println("------------------------");
+        System.out.println("Name " + name);
+        System.out.println("Age " + age);
+        System.out.println("Gender " + gender);
+
+    }
+}
