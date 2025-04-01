cdabbea17fac4e8254c18738df84919924285219
uKeps <piassinierik@gmail.com>
2025-04-01 18:30:12 -0300

    feature: sobrescrita do metodo toString

---
+++ src/maratona/javacore/Isobrescrita/domain/Anime.java
@@ -0,0 +1,24 @@
+package maratona.javacore.Isobrescrita.domain;
+
+public class Anime {
+    private String name;
+
+    @Override
+    public String toString() {
+        return "Anime{" +
+                "name='" + name + '\'' +
+                '}';
+    }
+
+    public Anime(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
---
+++ src/maratona/javacore/Isobrescrita/test/AnimeTest01.java
@@ -0,0 +1,10 @@
+package maratona.javacore.Isobrescrita.test;
+
+import maratona.javacore.Isobrescrita.domain.Anime;
+
+public class AnimeTest01 {
+    public static void main(String[] args) {
+        Anime anime = new Anime("Dr.Stone");
+        System.out.println(anime);
+    }
+}
