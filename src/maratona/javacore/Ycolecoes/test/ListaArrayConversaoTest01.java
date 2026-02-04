e5f2588d388cd65e3dfe8237cb6c86b4646f3239
uKeps <piassinierik@gmail.com>
2026-02-04 15:16:53 -0300

    feat: colecoes - conversao array list

---
+++ src/maratona/javacore/Ycolecoes/test/ListaArrayConversaoTest01.java
@@ -0,0 +1,36 @@
+package maratona.javacore.Ycolecoes.test;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class ListaArrayConversaoTest01 {
+    public static void main(String[] args) {
+        List<Integer> numbers = new ArrayList<>();
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+        Integer[] listToArray = numbers.toArray(new Integer[0]);
+        System.out.println(Arrays.toString(listToArray));
+        System.out.println("--------------------");
+
+        Integer[] numbersArray = new Integer[3];
+        numbersArray[0] =1;
+        numbersArray[1] =2;
+        numbersArray[2] =3;
+
+
+        List<Integer> arrayToList = Arrays.asList(numbersArray);
+        arrayToList.set(0,12);
+        System.out.println(Arrays.toString(numbersArray));
+        System.out.println(arrayToList);
+
+        System.out.println("-----------");
+        List<Integer> numbersList = new ArrayList<>(Arrays.asList(numbersArray));
+        numbersList.add(15);
+        System.out.println(numbersList);
+
+        List<String> strings = Arrays.asList("1", "2");
+    }
+}
