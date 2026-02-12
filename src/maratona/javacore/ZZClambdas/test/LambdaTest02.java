599551fef80fa5864a2e7d6690bea7a60726e6f8
uKeps <piassinierik@gmail.com>
2026-02-12 14:28:01 -0300

    feat: lambdas - function

---
+++ src/maratona/javacore/ZZClambdas/test/LambdaTest02.java
@@ -0,0 +1,25 @@
+package maratona.javacore.ZZClambdas.test;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Function;
+
+public class LambdaTest02 {
+    public static void main(String[] args) {
+        List<String> strings = List.of("Popeye", "Olivia Palito");
+        List<Integer> integers = map(strings, s -> s.length());
+        List<String> map = map(strings, s -> s.toUpperCase());
+        System.out.println(integers);
+        System.out.println(map);
+
+
+    }
+    private static <T, R> List<R> map(List<T> list, Function<T, R> function){
+        List<R> result = new ArrayList<>();
+        for(T e: list){
+            R r = function.apply(e);
+            result.add(r);
+        }
+        return result;
+    }
+}
