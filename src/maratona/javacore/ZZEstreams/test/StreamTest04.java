0714dbed07e578a10da09d4f804f1d1e67c7131d
uKeps <piassinierik@gmail.com>
2026-02-16 23:57:45 -0300

    feat: streams - flatmap

---
+++ src/maratona/javacore/ZZEstreams/test/StreamTest04.java
@@ -0,0 +1,26 @@
+package maratona.javacore.ZZEstreams.test;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+public class StreamTest04 {
+    public static void main(String[] args) {
+        List<List<String>> devdojo = new ArrayList<>();
+        List<String> graphicDesigners = List.of("Wildnei Suane", "Catarina Santos", "Sandy Carolina");
+        List<String> developers = List.of("William", "David", "Harisson");
+        List<String> students = List.of("Édipo", "Gustavo Lima", "Gustavo Mendes", "Guilherme");
+        devdojo.add(graphicDesigners);
+        devdojo.add(developers);
+        devdojo.add(students);
+
+        for (List<String> people : devdojo) {
+            for (String person : people) {
+                System.out.println(person);
+            }
+        }
+        System.out.println("----");
+        devdojo.stream().flatMap(Collection::stream).forEach(System.out::println);
+
+    }
+}
