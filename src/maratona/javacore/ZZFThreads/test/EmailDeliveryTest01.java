e31876fb1b7a105050c9fcdab0d0437629b10911
uKeps <piassinierik@gmail.com>
2026-02-25 14:59:12 -0300

    feat: Threads

---
+++ src/maratona/javacore/ZZFThreads/test/EmailDeliveryTest01.java
@@ -0,0 +1,24 @@
+package maratona.javacore.ZZFThreads.test;
+
+import maratona.javacore.ZZFThreads.dominio.Members;
+import maratona.javacore.ZZFThreads.service.EmailDeliveryService;
+
+import javax.swing.*;
+
+public class EmailDeliveryTest01 {
+    public static void main(String[] args) {
+        Members members = new Members();
+        Thread jiraya = new Thread(new EmailDeliveryService(members), "Jiraya");
+        Thread kakashi = new Thread(new EmailDeliveryService(members), "Kakashi");
+        jiraya.start();
+        kakashi.start();
+        while(true){
+            String email = JOptionPane.showInputDialog("Entre com seu email");
+            if(email == null || email.isEmpty()){
+                members.close();
+                break;
+            }
+            members.addMemberEmail(email);
+        }
+    }
+}
