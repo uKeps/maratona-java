0b1a89c23f7b466d0d5dbb6020eb4f88bb8d83a3
uKeps <piassinierik@gmail.com>
2026-02-26 16:18:48 -0300

    feat: JDBC - Dependencias maeven e conectando ao DB

---
+++ src/main/java/maratona/javacore/ZZIjdbc/test/ConnectionFactoryTest.java
@@ -0,0 +1,9 @@
+package maratona.javacore.ZZIjdbc.test;
+
+import maratona.javacore.ZZIjdbc.conn.ConnectionFactory;
+
+public class ConnectionFactoryTest {
+    public static void main(String[] args) {
+        ConnectionFactory.getConnection();
+    }
+}
